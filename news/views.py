6fe79a39fe8c61c96081f9a6c326fdb820adc606
Hassani <hassanisnr@gmail.com>
2019-03-24 22:25:38 -0400

    Fixed broken news links

--- news/views.py
+++ news/views.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from django.shortcuts import render
+from decouple import config
+from pytz import timezone
 
 from . models import News
 
@@ -8,6 +12,9 @@ def news(request):
     records = News.objects.all().order_by('-news_id')
 
     context = {
-        'news': list(records)
+        'news': list(records),
+        'now': datetime.now(tz=timezone(config('CELERY_TIMEZONE')))
     }
+
+    print(context)
     return render(request, 'pages/news.html', context)
